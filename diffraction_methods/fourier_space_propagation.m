9def8eb0d4820ee8142b255e616429808c954466
geoff22873 <geoffrey.evans.13@ucl.ac.uk>
2014-03-27 17:27:59 +0000

    angular spectrum propagation working

--- diffraction_methods/fourier_space_propagation.m
+++ diffraction_methods/fourier_space_propagation.m
@@ -1,37 +1,44 @@
 function run()
 
-fourier_space_propagation( @Gaussian, 3, -40, 40, 1, 5)
+fourier_space_propagation( @Gaussian, 0.1, -26, 20, 1, 5)
 
     function [] = fourier_space_propagation( ScalarFunction, xResolution, xStart, xEnd, k, zMax)
         
-        xSize = xEnd - xStart;
-        numSamples = 2^ceil(log2(xSize/xResolution));
-        kResolution = 2*pi/numSamples / xResolution;
+        numSamples = 2^ceil(log2((xEnd - xStart)/xResolution)); % round to even num
+        xLength = xResolution * numSamples;
+        xEnd = xStart + xLength;
         
-        xVals = xResolution * (0:numSamples-1);
-        kVals = kResolution * (0:numSamples-1);
-        zVals = 0:zMax;
+        [ kx ,ft ] = dft( ScalarFunction, xResolution, xStart, xEnd );
         
-        kSamples = fft(ScalarFunction(xVals));
-        propagatedSamples = PropagateFt(kSamples, kVals);
-        for n = 1:zMax+1
-            xSamples(n,:) = ifft(propagatedSamples(n,:));
+        zVals = 0:0.1:zMax;
+        ftPropagated = PropagateFt(ft, kx, zVals);
+        
+        xPropagated = zeros(zMax+1,numSamples);
+        for n = 1:length(zVals)
+            xPropagated(n,:) = ifft(ftPropagated(n,:));
         end
         
-        [x,z] = meshgrid(xVals,zVals);
-        halfNs = numSamples/2;
-        s = surf([x(:,halfNs:numSamples-1)-xSize,x(:,1:halfNs)],z,abs([xSamples(:,halfNs:numSamples-1),xSamples(:,1:halfNs)]));
+        xVals = 0:xResolution:(xLength-xResolution);
+        xVals = xVals - xLength * (xVals >= xEnd);
+        [xGrid,zGrid] = meshgrid(xVals,zVals);
+         
+        [~,ind] = sortrows(xGrid');
+        xGrid = xGrid(:,ind');
+        xPropagated = xPropagated(:,ind');
+        s = surf(xGrid,zGrid,abs(xPropagated));
+        
         set(s,'linestyle','none');
         xlabel('x')
         ylabel('z')
         zlabel('I')
         
-        function propagatedSamples = PropagateFt(ftSamples, kx)
+        function propagatedSamples = PropagateFt(ftSamples, kx, zVals)
             kz = sqrt(k.^2 - kx.^2);
             [kzGrid,zGrid] = meshgrid(kz,zVals);
             phase = exp(1i .* kzGrid .* zGrid);
-            propagatedSamples = repmat(ftSamples,zMax+1,1) .* phase;
+            propagatedSamples = repmat(ftSamples,length(zVals),1) .* phase;
         end
+         
         
     end
 
