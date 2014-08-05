16016ed6056386d0a1038ae44d186afa3fddb138
geoff22873 <geoffrey.evans.13@ucl.ac.uk>
2014-08-05 18:41:18 +0100

    almost have continuous aol animation

--- aod_drive/RayTraceParax2d.m
+++ aod_drive/RayTraceParax2d.m
@@ -6,38 +6,50 @@ V = [613, 700];
 l = [5e-2, 1];
 vx = 0;
 linearChirps = [FirstChirp, SecondChirp];
-[t, x1] = GetSamplingRanges();
-CalculateRayEnds();
+xRange = 0:0.001:0.01;
+tRange = -1.61e-5 * (0:10);
+L = 0.01;
+aodEnds = [0; 1] * L;
 
-    function [xEnd,aberrationVar] = CalculateRayEnds()
+for t = tRange
+    CalculateRayEnds(t, xRange, aodEnds);
+    pause(0.3)
+    clf
+end
+
+    function [xEnd,aberrationVar] = CalculateRayEnds(t, xRange, aodEnds)
         x = cell(1,numberOfAods+1); 
         z = cell(1,numberOfAods+1); 
-        x{1} = x1;
-        z{1} = 0;
+        x{1} = xRange;
+        z{1} = 0.1;
         
+        line([x{1};x{1}],[0;z{1}]);
         hold on
         for n = 1:numberOfAods
             x{n+1} = GetNextXyPositions(n);
             z{n+1} = z{n} + l(n);
             line([x{n};x{n+1}],[z{n};z{n+1}]);
-            line([max(x{n}) min(x{n})],repmat([z{n}],1,2))
+            line(aodEnds,repmat([z{n}],1,2))
         end
         hold off
         
         xEnd = x{end} - vx * t;
         aberrationVar = var(xEnd);
              
-        function xNplus1 = GetNextXyPositions(n)
+        function xNext = GetNextXyPositions(n)
             angle = NthDeflectionAngle(1);
             for m = 2:n % does this hit if n < 2
                 angle = angle + NthDeflectionAngle(m);
             end
-            xNplus1 = x{n} + angle(1,:) * l(n);
+            xCurrent = x{n};
+            xNext = xCurrent + angle(1,:) * l(n);
+            xNext(xCurrent > aodEnds(2)) = inf;
+            xNext(xCurrent < aodEnds(1)) = inf;
         end   
         
         function fnangle = NthDeflectionAngle(n) 
             phase = t - x{n} / V(n);
-            fnangle = linearChirps(n) * phase * lambda/V(n);
+            fnangle = ConstAngle(phase, n) + linearChirps(n) * ShiftPhase(phase, n) * lambda/V(n);
         end
     end
 
@@ -48,12 +60,20 @@ CalculateRayEnds();
     function c = SecondChirp()
         c = V(2)^2/lambda * (V(1) - vx)/(l(2)*V(1) - l(2)*V(2));
     end
-end
 
-function [t,x1] = GetSamplingRanges()
-    xRange = -0.01;
-    tRange = -1.61e-5;
-    [x1temp,ttemp] = meshgrid(xRange,tRange);
-    x1 = x1temp(:)';
-    t = ttemp(:)';
+    function val = ShiftPhase(phase, n)
+        factor = L / V(n);
+        num = ceil(phase / factor);
+        val = phase - num * factor;
+    end
+
+    function a = ConstAngle(phase, n)
+    	factor = L / V(n);
+        num = ceil(phase / factor);
+        if n == 2
+            a = num * L / l(n);
+        else
+            a = 0;
+        end
+    end
 end
