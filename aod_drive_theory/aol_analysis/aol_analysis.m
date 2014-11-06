c8558b9595008204560138daba94b3da98331c9a
geoff22873 <geoffrey.evans.13@ucl.ac.uk>
2014-11-06 14:28:45 +0000

    aberrations correction better

--- aod_drive_theory/aol_analysis/aol_analysis.m
+++ aod_drive_theory/aol_analysis/aol_analysis.m
@@ -2,31 +2,39 @@ function aol_analysis(focalLength, correction, angleSamples, aolFunction)
 
 lambda = 800e-9;
 V = 613;
-xVel = 0;
-yVel = 0;
+vx = 0;
+vy = 0;
 [t, x1, y1, k1] = GetSamplingRanges(lambda, angleSamples);
 
 [x, y, z, k] = CalculateRayEnds(t, x1, y1, k1, focalLength, correction, aolFunction);
-[x2, y2, k2] = Relay(x{end-1}, y{end-1}, k{end}, 0.5);
+[x2, y2, k2] = Relay(x{end-1}, y{end-1}, k{end}, 0.5, 0.0);
 [x3, y3, k3] = Objective(x2, y2, k2);
 [xF, yF, d] = GetFocusedXy(k3, x3, y3);
 
+%PlotAolFocus(x,y,z,k,t)
+%PlotAol(x,y,z,k)
 PlotObjFocus(x3, y3, xF, yF, k3, d, t)
+PlotAroundZeroPlane(k3,x3,y3,5e-4)
 %TraceRays(x, y, z, k{end})
 %TraceRays2D(x, y, z, k{end})
 
     function PlotObjFocus(x3, y3, xF, yF, k3, d, t)
-        TraceRays2D({x3, xF}, {y3, yF}, {0, d}, k3);
+        %TraceRays2D({x3, xF}, {y3, yF}, {0, d}, k3);
         XyScatterPsf(xF, yF, 0, 0, t);
+        
+        zIntercepts = x3 .* k3(3,:)./k3(1,:); % projected into xz plane
+        
+        zFocalPoint = mean(zIntercepts)
+        zStd = std(zIntercepts - zFocalPoint)
     end
 
     function PlotAolFocus(x,y,z,k,t)
-        TraceRays(1, {x{end-1}, x{end}}, {y{end-1}, y{end}}, {z{end-1}, z{end}}, k{end});
+        TraceRays({x{end-1}, x{end}}, {y{end-1}, y{end}}, {z{end-1}, z{end}}, k{end});
         XyScatterPsf(x{end}, y{end}, 0, 0, t);
     end
         
     function PlotAol(x,y,z,k)
-        TraceRays(6, x, y, z, k{end});
+        TraceRays(x, y, z, k{end});
     end
 
     function ViewCrossSections(x,y,x2,y2,x3,y3,t)
@@ -38,7 +46,7 @@ PlotObjFocus(x3, y3, xF, yF, k3, d, t)
     function PlotAroundZeroPlane(k3,x3,y3,D)
         [xS, yS] = GetNextXy(k3, x3, y3, -D);
         [xE, yE] = GetNextXy(k3, x3, y3, D);
-        TraceRays(1, {xS, xE}, {yS, yE}, {-D, D}, k3);
+        TraceRays2D({xS, xE}, {yS, yE}, {-D, D}, k3);
     end
 
     function [x, y, z, k] = CalculateRayEnds(t, x1, y1, k1, focalLength, cubicChirp, aolFunction)
@@ -87,9 +95,9 @@ function [xOut, yOut, d] = GetFocusedXy(k, x, y)
     [xOut, yOut] = GetNextXy(k, x, y, d);
 end
 
-function [x2, y2, k2] = Relay(x, y, k, mag)
-    x2 = -x*mag;
-    y2 = -y*mag;
+function [x2, y2, k2] = Relay(x, y, k, mag, fac)
+    x2 = -x*mag - fac*k(1,:)./k(3,:);
+    y2 = -y*mag - fac*k(2,:)./k(3,:);
     k2 = k;
     k2(1:2,:) = -k(1:2,:)./mag;
 end
@@ -107,6 +115,9 @@ function [x2, y2, k2] = Objective(x, y, k)
     k2 = ConvertAngleToWavevec(angleOut, theta);
     x2 = r2 .* cos(phi);
     y2 = r2 .* sin(phi);
+    if ~isreal(k2)
+        error('make relay mag smaller')
+    end
 end
 
 function XyScatterPsf(x, y, xVel, yVel, t)
@@ -116,10 +127,11 @@ function XyScatterPsf(x, y, xVel, yVel, t)
     yE = yEff(:);
     figure();
     scatter(xE, yE);
-    axis equal
+    r = 2e-5;
+    %axis([-r r -r r])
     xlabel('x')
     ylabel('y')
-    display(var(xEff) + var(yEff)) % sum x and y vars
+    xyStd = sqrt(var(xEff) + var(yEff)) % sum x and y vars
 end
 
 function TraceRays2D(x, y, z, kEnd)
@@ -127,11 +139,11 @@ function TraceRays2D(x, y, z, kEnd)
     figure();
     hold on
     for n = 1:numberOfAods
-        line([x{n};x{n+1}],[z{n};z{n+1}]);
+        line([x{n};x{n+1}], [z{n};z{n+1}], 'color', 'red');
     end
     extra = 0.3 * (z{n+1} - z{n});
     [xNext, ~] = GetNextXy(kEnd, x{n+1}, y{n+1}, extra);
-    line([x{n+1};xNext],[z{n+1};z{n+1}+extra]);
+    line([x{n+1};xNext],[z{n+1};z{n+1}+extra], 'color', 'red');
     hold off
     alpha(0.1)
 end
@@ -156,8 +168,8 @@ end
 function [t,x1,y1,k1] = GetSamplingRanges(lambda, angleSamples)
     thetaRange = linspace(0,2*pi,angleSamples);
     thetaRange = thetaRange(2:length(thetaRange));
-    rRange = linspace(-10,10,12) * 1e-3;
-    tRange = 0 * 1e-6;
+    rRange = linspace(-10,10,8) * 1e-3;
+    tRange = [-5] * 1e-6;
     xTemp = repmat(cos(thetaRange') * rRange, [1, 1, length(tRange)]);
     yTemp = repmat(sin(thetaRange') * rRange, [1, 1, length(tRange)]);
     tTemp = zeros([length(thetaRange), length(rRange), length(tRange)]);
--- aod_drive_theory/aol_analysis/correction_comparison.m
+++ aod_drive_theory/aol_analysis/correction_comparison.m
@@ -1,33 +1,78 @@
 function correction_comparison()
 
-NoCorrection6aol()
-GoodCorrection6aol()
+%FocalPlane4aol()
+%NoCorrection6aolLong()
+GoodCorrection6aolLong()
+%NoCorrection4aolLong()
+%PoorCorrection4aolLong()
 
 end
 
 function NoCorrection4aol()
+correction =  0;
+focalLength = 1e0;
+angleSamples = 8;
+aolFunction = @aol_chirps.Aod4;
+aol_analysis(focalLength, correction, angleSamples, aolFunction)
+end
+function PoorCorrection4aol()
+correction =  13e19;
+focalLength = 1e0;
+angleSamples = 8;
+aolFunction = @aol_chirps.Aod4;
+aol_analysis(focalLength, correction, angleSamples, aolFunction)
+end
+function GoodCorrection6aol()
+correction =  10.4e19;
+focalLength = 1e0;
+aolFunction = @aol_chirps.Aod6pairedAS;
+angleSamples = 8;
+aol_analysis(focalLength, correction, angleSamples, aolFunction)
+end
+function NoCorrection6aol()
+correction =  0;
+focalLength = -1e0;
+aolFunction = @aol_chirps.Aod6pairedAS;
+angleSamples = 8;
+aol_analysis(focalLength, correction, angleSamples, aolFunction)
+end
+function FocalPlane6aol()
+correction =  0;
+focalLength = 1e24;
+aolFunction = @aol_chirps.Aod6pairedAS;
+angleSamples = 8;
+aol_analysis(focalLength, correction, angleSamples, aolFunction)
+end
+function FocalPlane4aol()
+correction =  0;
+focalLength = 1e24;
+aolFunction = @aol_chirps.Aod4;
+angleSamples = 8;
+aol_analysis(focalLength, correction, angleSamples, aolFunction)
+end
+function NoCorrection4aolLong()
 correction =  0;
 focalLength = 1e1;
 angleSamples = 8;
 aolFunction = @aol_chirps.Aod4;
 aol_analysis(focalLength, correction, angleSamples, aolFunction)
 end
-function PoorCorrection4aol()
+function PoorCorrection4aolLong()
 correction =  2.7e19;
 focalLength = 1e1;
 angleSamples = 8;
 aolFunction = @aol_chirps.Aod4;
 aol_analysis(focalLength, correction, angleSamples, aolFunction)
 end
-function NoCorrection6aol()
-correction =  0;
+function GoodCorrection6aolLong()
+correction = 2.2e19; %2.2
 focalLength = 1e1;
 aolFunction = @aol_chirps.Aod6pairedAS;
 angleSamples = 8;
 aol_analysis(focalLength, correction, angleSamples, aolFunction)
 end
-function GoodCorrection6aol()
-correction =  2.3e19;
+function NoCorrection6aolLong()
+correction =  0;
 focalLength = 1e1;
 aolFunction = @aol_chirps.Aod6pairedAS;
 angleSamples = 8;
