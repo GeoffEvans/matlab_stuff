16182de1bbf8970df6b0dd79064d26ce5f814ea4
Geoff Evans <geoffrey.evans.13@ucl.ac.uk>
2015-04-30 16:05:30 +0100

    calculate ac walkoff

---
+++ aol_model/ac_walkoff.m
@@ -0,0 +1,11 @@
+function [ walkoff ] = ac_walkoff( rotation )
+
+rotation = rotation / 180 * pi;
+dTh = 1e-9;
+V = teo2.find_v_ac_min( pi/2+rotation+[0,dTh], pi/4+[0,0]);
+dV = V(2) - V(1);
+
+walkoff = dV / V(1) / dTh * 180 / pi;
+
+end
+
