a477023e8ca16fb56aeae9680f636a7d20df9323
Geoff Evans <geoffrey.evans.13@ucl.ac.uk>
2015-07-06 16:16:57 +0100

    tuned up fft model for NA

--- @aol_fft/aol_fft.m
+++ @aol_fft/aol_fft.m
@@ -3,9 +3,9 @@ classdef aol_fft
     properties
         adjustment1 = 5e2; % ratio of k to max(kx)
         adjustment2 = 1e0;
-        number_of_samples = 2.^10 - 1; % computational speed vs accuracy
-        k = 2*pi/920e-9;
-        z_list = linspace(-20,80,99)*1e-6;
+        number_of_samples = 2.^8 - 1; % computational speed vs accuracy
+        k = 2*pi/850e-9;
+        z_list = linspace(60,90,99)*1e-6;
         do_plot = struct('input', 0, 'focal_plane', 0, 'angular_spec', 0);
         V = 613;
     end
@@ -16,9 +16,43 @@ classdef aol_fft
             [propagated_wave, x, y] = obj.calculate_psf(sampled_wave_2d, space_width);
         end
         
+        function [sampled_wave, space_width] = get_sampled_wavefunction(obj, waves, t)     
+            num_aods = numel(waves.aods);
+            space_width = pi * obj.number_of_samples / obj.k * obj.adjustment1;
+            samples = linspace(-1/2, 1/2, obj.number_of_samples) * space_width;
+            [x, y] = meshgrid(samples);
+
+            distances = [0, ones(1,num_aods-1)*0.0]; %qq
+            directions = linspace(0, 2 * pi, num_aods + 1);
+            T = obj.V * t;
+            r = arrayfun(@(theta) x.*cos(theta) + y.*sin(theta), directions, 'uniformoutput', 0);
+            envelope = exp(-(x.^2 + y.^2)/2/(4e-3).^2);
+            labels = {'x', 'y'};
+            
+            sampled_wave = envelope;
+            for n = 1:num_aods
+                phase = 2*pi * waves.aods{n} ./ (6e-3).^(1:5); % take aperture to be 12mm because of scaling by relay
+                sampled_wave = obj.propagate_wave(sampled_wave, distances(n), space_width, 1)...
+                    .* exp(1i * (phase(1) .* (r{n}-T) + phase(2) .* (r{n}-T).^2 + phase(3) .* (r{n}-T).^3 + phase(4) .* (r{n}-T).^4));
+                if obj.do_plot.input
+                    obj.plot_wavefunction_2d(sampled_wave, x, y, labels)
+                end
+            end
+
+            focal_length = inv(num_aods * phase(2) / obj.k)
+            sampled_wave = sampled_wave .* (sqrt(x.^2 + y.^2) < 6e-3) .* exp(1i * (...  
+                (x.^2 + y.^2) * waves.focus * 2*pi ./ (6e-3).^2 ./ 2 ...  
+                + tand(waves.ac_angle) * 0.5 .* (x.^3 + y.^3) .* focal_length.^-2  * obj.k/(2*pi)...  
+                + (x.^2 + y.^2).^2 ./ (6e-3).^4 * waves.spherical * 2*pi)); 
+            
+            if obj.do_plot.input
+                obj.plot_wavefunction_2d(sampled_wave, x, y, labels)
+            end
+        end
+        
         function [propagated_wave, x, y] = calculate_psf(obj, sampled_wave_2d, space_width)
             [x, y, focal_plane_wave_2d, space_width] = obj.get_focal_plane_wavefunction(sampled_wave_2d, space_width);
-            propagated_wave = obj.propagate_wave(focal_plane_wave_2d, obj.z_list, space_width);
+            propagated_wave = obj.propagate_wave(focal_plane_wave_2d, obj.z_list, space_width, 4/3);
         end
 
         function [u, v, focal_plane_wave_2d, space_width] = get_focal_plane_wavefunction(obj, sampled_wave_2d, space_width)
@@ -31,13 +65,13 @@ classdef aol_fft
             end
         end
         
-        function propagated_wave = propagate_wave(obj, wave_2d, distance, space_width)
+        function propagated_wave = propagate_wave(obj, wave_2d, distance, space_width, ref_ind)
             count = [0:floor((obj.number_of_samples - 1)/2) floor(-(obj.number_of_samples - 1)/2):-1];
             [k_x, k_y] = meshgrid(count * 2 * pi / space_width);
             ft_wave_2d = fft2(ifftshift(wave_2d)); % recentre wave with fftshift
 
-            %display(max(k_x(:))/k); display((k_y(2)-k_y(1))/k);
-            k_z = sqrt(obj.k.^2 - k_x.^2 - k_y.^2);
+            %display(max(k_x(:))/obj.k); display((k_y(2)-k_y(1))/obj.k);
+            k_z = sqrt(obj.k.^2 * ref_ind.^2 - k_x.^2 - k_y.^2);
             mask = ~(abs(imag(k_z)) > 0);
 
             propagated_wave = zeros([size(k_z), numel(distance)]);
@@ -66,51 +100,17 @@ classdef aol_fft
             set(hh,'EdgeColor','none')
         end
         
-        function xy_xz_plot_psf(obj, propagated_wave_2d, x, y, z_plane_frac, x_plane_frac)
+        function xy_xz_plot_psf(obj, propagated_wave_2d, x, y, z_plane_frac)
             figure(); 
             subplot(1,2,1)
-            h = pcolor(x, y, abs(propagated_wave_2d(:,:,round(z_plane_frac*size(obj.z_list,1)))));
+            h = pcolor(x, y, abs(propagated_wave_2d(:,:,round(z_plane_frac*size(obj.z_list,1)))).^1);
             set(h,'EdgeColor','none')
             axis equal
 
             subplot(1,2,2)
-            [zz, xx] = meshgrid(obj.z_list, x(round(x_plane_frac*size(x,1)),:));
-            h = pcolor(xx, zz, abs(squeeze(propagated_wave_2d(:,round(size(x,2)/2),:))));
+            [zz, xx] = meshgrid(obj.z_list, max(x,[],1));
+            h = pcolor(xx, zz, abs(squeeze(propagated_wave_2d(:,round(size(x,2)/2),:))).^1);
             set(h,'EdgeColor','none')
-        end
-        
-        function [sampled_wave, space_width] = get_sampled_wavefunction(obj, waves, t)     
-            space_width = pi * obj.number_of_samples / obj.k * obj.adjustment1;
-            samples = linspace(-1/2, 1/2, obj.number_of_samples) * space_width;
-            [x, y] = meshgrid(samples);
-
-            scale_factors = 2*pi / obj.k ./ (6e-3).^(1:5); % take aperture to be 12mm because of scaling by relay
-            distances = [0, ones(1,numel(waves.aods)-1)*0.05];
-            directions = linspace(0, 2 * pi, numel(waves.aods) + 1);
-            T = obj.V * t;
-            r = arrayfun(@(theta) x.*cos(theta) + y.*sin(theta), directions, 'uniformoutput', 0);
-            envelope = exp(-(x.^2 + y.^2)/2/(6e-3).^2);% .* (sqrt(x.^2 + y.^2) < 5e-3);
-            labels = {'x', 'y'};
-            
-            sampled_wave = envelope;
-            for n = 1:numel(waves.aods)
-                phase = obj.k * (waves.aods{n} .* scale_factors);
-                sampled_wave = obj.propagate_wave(sampled_wave, distances(n), space_width)...
-                    .* exp(1i * (phase(1) .* (r{n}-T) + phase(2) .* (r{n}-T).^2 + phase(3) .* (r{n}-T).^3 + phase(4) .* (r{n}-T).^4));
-                if obj.do_plot.input
-                    obj.plot_wavefunction_2d(sampled_wave, x, y, labels)
-                end
-            end
-            
-            focal_length = inv(4 * phase(2) / obj.k)
-            sampled_wave = sampled_wave .* exp(1i * (...  
-                (x.^2 + y.^2) * waves.focus * 2*pi ./ (6e-3).^2 ./ 2 ...  
-                + tand(waves.ac_angle) * 0.5 .* (x.^3 + y.^3) .* focal_length.^-2  * obj.k/(2*pi)...  
-                + (x.^2 + y.^2).^2 * waves.spherical * 2*pi ./ (6e-3).^4 ./ 4)); 
-            
-            if obj.do_plot.input
-                obj.plot_wavefunction_2d(sampled_wave, x, y, labels)
-            end
         end
     end
 end
--- diffraction_methods/psf_4.m
+++ diffraction_methods/psf_4.m
@@ -1,10 +1,10 @@
 function psf_4()
 tic
     time = 0e-6;
-    x1 = [0, 1, 0, 0, 0];
-    y1 = [0, 1, 0, 0, 0];    
-    x2 = [0, 1, 0, 0, 0];
-    y2 = [0, 1, 0, 0, 0];    
+    x1 = [0, 10, 0, 0, 0];
+    y1 = [0, 10, 0, 0, 0];    
+    x2 = [0, 10, 0, 0, 0];
+    y2 = [0, 10, 0, 0, 0];    
     
     waves = struct();
     waves.focus = 0;
@@ -14,7 +14,22 @@ tic
     
     a = aol_fft();
     [propagated_wave, x, y] = a.calculate_psf_through_aol(waves, time);
-    a.xy_xz_plot_psf(propagated_wave, x, y, 0.5, 0.5);
+    a.xy_xz_plot_psf(propagated_wave, x, y, 0.5);
+    get_psf_dimensions(propagated_wave, x, y, a.z_list)
 toc
 end
 
+function res = get_psf_dimensions(field_3d, x, y, z)
+    r = sqrt(x.^2 + y.^2);
+    max_intensity_sqr = max(abs(field_3d(:)).^4)
+    [row, col, depth] = find(field_3d == max_intensity_sqr);
+    display(2 * [row/size(field_3d,1), col/size(field_3d,2), depth/size(field_3d,3)]) % should print 1 1 1
+    
+    half_or_more_r = max(abs(field_3d), [], 3).^4 >= max_intensity_sqr/2;
+    r_res = 2 * max(r(half_or_more_r));
+    
+    half_or_more_z = squeeze(max(max(abs(field_3d)))).^4 >= max_intensity_sqr/2;
+    z_res = max(z(half_or_more_z)) - min(z(half_or_more_z));
+    
+    res = [r_res, z_res] * 1e6;
+end
