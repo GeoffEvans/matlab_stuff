4c2d7b319ed3d15bb154e5fc55bb831ded599270
Geoff Evans <geoffrey.evans.13@ucl.ac.uk>
2015-11-24 16:46:42 +0000

    pkgk figs

--- diffraction_methods/figB.m
+++ diffraction_methods/figB.m
@@ -1,6 +1,11 @@
 ws = -10;
-t = (-1:1:1)*5e-6;
-psf_6(t, 0, 0, 0, 0, 0) % no aberration
-psf_6(t, 0, 0, 0, 0, ws) % spherical aberration
-psf_4(t, 0, 0, 0, 7, ws) % instantaneous correction using 4-AOD
-psf_6(t, 0, 0, 0, 5, ws) % instantaneous correction using 6-AOD
+time = (-1:1:1)*0.25e-6;
+aol = aol_fft();
+aol.z_list = linspace(-50,50,199)*1e-6;
+aol.pwr = 1;
+aol.cm = cm; % jet;
+
+psf_6(aol, time, 0, 0, 0, 0, 0, 0.5) % no aberration
+psf_6(aol, time, 0, 0, 0, 0, ws, 0.39) % spherical aberration
+psf_4(aol, time, 0, 0, 0, 5, ws, 0.47) % correction using 4-AOD
+psf_6(aol, time, 0, 0, 0, 4.7, ws, 0.5) % correction using 6-AOD
