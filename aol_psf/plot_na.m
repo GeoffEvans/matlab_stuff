3f2d3b017c74b16ac885c9de1a2a2611342aa95a
Geoff Evans <geoffrey.evans.13@ucl.ac.uk>
2016-02-09 18:16:15 +0000

    extend aol_psf to get fluorescence

--- aol_psf/plot_na.m
+++ aol_psf/plot_na.m
@@ -1,4 +1,4 @@
-function [x_fwhm, z_fwhm] = plot_na(aol, NA)
+function [x_fwhm, z_fwhm, max_fl, total_fl] = plot_na(aol, NA)
 
 aol.half_width = NA * 1e-2;
 aol.beam_sigma = NA/2 * 1e-2; 
@@ -11,6 +11,8 @@ x_list = 0:50:250;
 points = size(x_mesh);
 z_fwhm = zeros(points);
 x_fwhm = zeros(points);
+max_fl = zeros(points);
+total_fl = zeros(points);
 
 for m = 1:points(1)
     for n = 1:points(2)
@@ -23,12 +25,21 @@ for m = 1:points(1)
         res = psf_4_linear(aol, 0, [x_aol, 0], z_aol, [0,0], 0, [0,0,0,0], 0, 0, 0, false);
         x_fwhm(m,n) = res(3);
         z_fwhm(m,n) = res(4);
+        max_fl(m,n) = res(5);
+        total_fl(m,n) = res(6);
     end
 end
 x_mesh = [fliplr(-x_mesh), x_mesh; fliplr(-x_mesh), x_mesh];
 z_mesh = [flipud(-z_mesh), flipud(-z_mesh); z_mesh, z_mesh];
-x_fwhm = [flipud(fliplr(x_fwhm)), flipud(x_fwhm); fliplr(x_fwhm), x_fwhm];
-z_fwhm = [flipud(fliplr(z_fwhm)), flipud(z_fwhm); fliplr(z_fwhm), z_fwhm];
+x_fwhm = expand(x_fwhm);
+z_fwhm = expand(z_fwhm);
+max_fl = expand(10.^(max_fl - max(max_fl(:))));
+total_fl = expand(10.^(total_fl - max(total_fl(:))));
 
 ellipse_grid(x_mesh, z_mesh, x_fwhm, z_fwhm);
+ellipse_grid(x_mesh, z_mesh, max_fl, total_fl);
+end
+
+function expanded = expand(mat)
+    expanded = [rot90(mat,2), flipud(mat); fliplr(mat), mat];
 end
