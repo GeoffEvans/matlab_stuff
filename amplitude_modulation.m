dbe8452bbf5c93b616317be525afd60c0558b4cd
Geoff Evans <geoffrey.evans.13@ucl.ac.uk>
2016-02-15 16:00:20 +0000

    amp mod plots for deviation from Bragg

--- amplitude_modulation.m
+++ amplitude_modulation.m
@@ -2,16 +2,37 @@ function amplitude_modulation()
     transducer_width = 3.2e-3;
 
     figure; hold on;
-    xlabel('incidence angle')
-    ylabel('efficiency')
-    rads = linspace(0, 20e-3);
-    powers = linspace(8, 0);
+    xlabel('Deviation from Bragg angle (degrees)')
+    ylabel('Diffraction efficiency')
+    rads = linspace(0, 23.52e-3);
+    angular_deviation = rads/2/pi*360*2.226;
+    powers = linspace(12, 0);
+    max_effs = zeros(size(rads));
+    max_effs_low = zeros(size(rads));
     for p = powers
         amp = amplitude(p, transducer_width);
         effs = get_eff(rads, amp, transducer_width);
-        plot(rads/2/pi*360*2.226, effs, 'Color', [(p > 2) && (p < 4), (p > 4) && (p < 6), (p < 2)]);
+        plot(angular_deviation, effs, 'Color', [(p > 2) && (p <= 4) || (p > 8) && (p <= 10), (p > 4) && (p <= 8), (p < 2) || (p > 6) && (p <= 8) || (p > 2) && (p <= 4)]);
+        max_effs = max(effs, max_effs);
+        if p < 4
+            max_effs_low = max(effs, max_effs_low);
+        end
     end
+    set(gca,'TickDir','out')
+    figure()
+    hold on
+    plot1 = plot(angular_deviation, max_effs, 'r');
+    plot2 = plot(angular_deviation, get_eff(rads, amplitude(10.5, transducer_width), transducer_width), 'r-');
+    plot3 = plot(angular_deviation, max_effs_low, 'b');
+    plot4 = plot(angular_deviation, get_eff(rads, amplitude(1.25, transducer_width), transducer_width), 'b-');
+    set(plot1,'Color',[0.6 0 0], 'linewidth', 2);
+    set(plot2,'Color',[1 0.4 0.4], 'linewidth', 2);
+    set(plot3,'Color',[0 0 1], 'linewidth', 2);
+    set(plot4,'Color',[0.3 0.75 0.93], 'linewidth', 2);
+    set(gca,'TickDir','out')
 end
+
+    
 function eff = get_eff(angles, amp, transducer_width)
     
     wavelength = 800e-9;
