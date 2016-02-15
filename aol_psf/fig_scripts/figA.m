62e8fa74419e903c5761afcc585f33a1042c1042
Geoff Evans <geoffrey.evans.13@ucl.ac.uk>
2016-02-15 16:00:09 +0000

    remove old aol_psf from general (has own repo)

--- aol_psf/fig_scripts/figA.m
+++
@@ -1,8 +0,0 @@
-time = (-10:1:10) * 1e-6;
-aol = aol_fft();
-aol.z_list = linspace(-20,20,299)*1e-6;
-aol.cm = cm; % jet;
-
-psf_4(aol, time, 0, 4, 0, 0, 0, 0.5); % x
-psf_4(aol, time, 0, 4, 0.8, 0, 0, 0.5); % xz
-psf_4(aol, time, 1.3, 4, 0, -0.5, 0, 0.5); % curved
